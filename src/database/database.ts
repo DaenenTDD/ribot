1703e6f10661e1b6de65095add47c669a6b59167
DaenenTDD <daenen2006@gmail.com>
2026-05-14 05:42:34 -0600

    make sure process crashes before silently connecting to in memory db

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,3 +1,7 @@
 import { drizzle } from "drizzle-orm/libsql";
 
-export const db = drizzle(process.env.DB_FILE_NAME!);
+if (!process.env.DB_FILE_NAME) {
+    throw new Error("DB_FILE_NAME is undefined. Did you import the db somewhere before running initEnv?")
+}
+
+export const db = drizzle(process.env.DB_FILE_NAME);
